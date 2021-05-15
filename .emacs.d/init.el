6465dea6e054236cc63fc6a2b21aa7b0ec9d5a87
Martin Skøtt <martin@skoett.name>
2021-05-15 23:09:42 +0200

    Basic init.el and README

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+;; load and configure package manager
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(markdown-mode org magit htmlize)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ README.md
@@ -0,0 +1,12 @@
+# Martins dotfiles #
+
+Inspiration:
+
+- https://github.com/FrostyX/dotfiles
+
+## EMACS ##
+
+Modes:
+
+- [Markdown Mode](https://jblevins.org/projects/markdown-mode/)
+
