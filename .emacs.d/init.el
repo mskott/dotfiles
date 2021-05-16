c34783b872ea0241aee4a8852a0fd932ec9ed01f
Martin Skøtt <martin@skoett.name>
2021-05-16 21:04:29 +0200

    Various changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,7 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package markdown-mode org magit htmlize)))
+ '(package-selected-packages '(toc-org use-package markdown-mode org magit htmlize)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
