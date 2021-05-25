2bd7fd341da34ac481289d2a372f9fb166ba67b1
Martin Skøtt <martin@skoett.name>
2021-05-25 23:02:19 +0200

    Move customized settings out of init.el and Git

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,16 +18,6 @@
 (org-babel-load-file (expand-file-name "martin.org" user-emacs-directory))
 (garbage-collect)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ws-butler ws-buffer olivetti toc-org use-package markdown-mode org magit htmlize)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Keep customized settings out of Git
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+(load custom-file)
